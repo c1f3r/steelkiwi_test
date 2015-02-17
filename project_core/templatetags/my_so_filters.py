da6d978df46862bd42e674505bc3c1ce35aae6c5
cifer <artem.antsyferov@gmail.com>
2015-02-17 11:39:50 +0200

    Edited filter

--- project_core/templatetags/my_so_filters.py
+++ project_core/templatetags/my_so_filters.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter(name='get_next')
 def add_class(value):
-    return value[value.find('?next=') + len('?next='):-1] + '/'
+    return value[value.find('?next=') + len('?next='):]
