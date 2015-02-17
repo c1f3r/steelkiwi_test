f085efa0dae151e1bd2f47db974e063f87bc7666
cifer <artem.antsyferov@gmail.com>
2015-02-17 11:28:28 +0200

    Made Email in registration unique

--- steelkiwi_test/urls.py
+++ steelkiwi_test/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.default.views import RegistrationView
+from registration.forms import RegistrationFormUniqueEmail
 
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'steelkiwi_test.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
+                       url(r'^accounts/register/$',
+                           RegistrationView.as_view(
+                               form_class=RegistrationFormUniqueEmail),
+                           name='registration_register'),
                        url(r'^accounts/',
                            include('registration.backends.default.urls')),
+
 
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^', include('mySO.urls')),
