8ff497e5871d190a3f13b71e1ae65b2ae0cf8f0b
cifer <artem.antsyferov@gmail.com>
2015-02-17 19:18:07 +0200

    Edited signal for using not with django-comments

--- mySO/signals.py
+++ mySO/signals.py
@@ -4,5 +4,5 @@ from django.core.mail import send_mail
 def notify_author_about_new_comment(sender, instance, **kwargs):
     send_mail('You have new comment on mySO site',
               '{0} posted comment on your "{1}" question'.format(
-                  instance.user_name, instance.content_object.subject),
-              'yacifer@gmail.com', instance.content_object.author.email)
+                  instance.author.username, instance.question.subject),
+              'ciferstov@gmail.com', instance.question.author.email)
